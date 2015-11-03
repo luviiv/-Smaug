63726689038b8757ce13a893ac43c9238a45a29a
luviiv <lus.lotus@gmail.com>
2015-11-03 15:56:49 +0800

    add babel to support multi language

--- Smaug/application.py
+++ Smaug/application.py
@@ -6,10 +6,19 @@
     :copyright: (c) 2015 by Lu Tianchao.
     :license: Apache, see LICENSE for more details.
 """
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 #flask library
-from flask import Flask
+from flask import Flask, request, jsonify
+#flaskext
+from flask.ext.babel import Babel, gettext as _
 #Smaug
 from Smaug import views
+from Smaug.config import DefaultConfig
+
+__all__ = ["create_app"]
 
 DEFAULT_APP_NAME = "Smaug"
 
@@ -19,6 +28,7 @@ DEFAULT_MODULES = (
 
 def create_app(config=None, app_name=None, modules=None):
     """create app for server"""
+
     if app_name is None:
         app_name = DEFAULT_APP_NAME
 
@@ -26,10 +36,75 @@ def create_app(config=None, app_name=None, modules=None):
         modules = DEFAULT_MODULES
 
     app = Flask(app_name)
+
+    configure_app(app, config)
+
+    configure_logging(app)
+    configure_extensions(app)
     configure_modules(app, modules)
+
     return app
+
+def configure_app(app, config):
+    """read configuration"""
+    app.config.from_object(DefaultConfig())
+
+    if config is not None:
+        app.config.from_object(config)
+
+    app.config.from_envvar('APP_CONFIG', silent=True)
+
+def configure_extensions(app):
+    """all extensions goes here"""
+    configure_i18n(app)
 
 def configure_modules(app, modules):
     """load modules that want to be used"""
+
     for module, url_prefix in modules:
         return app.register_blueprint(module, url_prefix=url_prefix)
+
+def configure_i18n(app):
+    """configure language setting"""
+
+    babel = Babel(app)
+
+    @babel.localeselector
+    def get_locale():
+        """
+            change here,
+            if you want to support multi locale
+        """
+        return app.config['BABEL_DEFAULT_LOCALE']
+
+def configure_logging(app):
+    if app.debug or app.testing:
+        return
+
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]')
+
+    debug_log = os.path.join(app.root_path, 
+                             app.config['DEBUG_LOG'])
+
+    debug_file_handler = \
+        RotatingFileHandler(debug_log,
+                            maxBytes=100000,
+                            backupCount=10)
+
+    debug_file_handler.setLevel(logging.DEBUG)
+    debug_file_handler.setFormatter(formatter)
+    app.logger.addHandler(debug_file_handler)
+
+    error_log = os.path.join(app.root_path, 
+                             app.config['ERROR_LOG'])
+
+    error_file_handler = \
+        RotatingFileHandler(error_log,
+                            maxBytes=100000,
+                            backupCount=10)
+
+    error_file_handler.setLevel(logging.ERROR)
+    error_file_handler.setFormatter(formatter)
+    app.logger.addHandler(error_file_handler)
---
+++ Smaug/config.py
@@ -0,0 +1,32 @@
+#encoding: utf-8
+"""
+    config.py
+    ~~~~~~~~~~~
+    :copyright: (c) 2015 by Lu Tianchao.
+    :license: Apache, see LICENSE for more details.
+"""
+
+class DefaultConfig(object):
+    """
+    Default configuration.
+    """
+
+    DEBUG = True
+
+    # change this in your production settings !!!
+
+    SECRET_KEY = "secret"
+
+    # keys for localhost. Change as appropriate.
+
+    RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+    RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///smaug.db"
+
+    SQLALCHEMY_ECHO = False
+
+    BABEL_DEFAULT_LOCALE = 'zh_Hans_CN'
+
+    DEBUG_LOG = 'logs/debug.log'
+    ERROR_LOG = 'logs/error.log'
